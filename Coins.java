1ddd2d43553d520824644abb85ed0f7b7ef479f3
Daniil Genin <daniil.genin@post.runi.ac.il>
2023-12-26 00:48:11 +0300

    Add awesome code for Homework 1

--- Coins.java
+++ Coins.java
@@ -4,6 +4,9 @@
  */
 public class Coins {
 	public static void main(String[] args) {
-		// Put your code here
+		int a = Integer.parseInt(args[0]);
+  		int quarter = a / 25;
+  		int cent = a - (quarter * 25);
+        System.out.println("Use " + quarter + " quarters and " + cent + " cents");
 	}
 }
--- LinearEq.java
+++ LinearEq.java
@@ -5,5 +5,17 @@
  * Treats the three arguments as well as the computed value as double values
  */
 public class LinearEq {
-	// Put your code here
+	public static void main(String[] args) {	
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int c = (int)(Math.random() * (a - b) + b);
+		int c1 = (int)(Math.random() * (a - b) + b);
+		int c2 = (int)(Math.random() * (a - b) + b);
+		System.out.println(c);
+		System.out.println(c1);
+		System.out.println(c2);
+		int mini = Math.min(c, c1);
+		int minimum = Math.min(mini, c2);
+		System.out.println("The minimal generated item was " + minimum);
+	}
 }
